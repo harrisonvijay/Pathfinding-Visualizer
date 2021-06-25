962157b487085e75f7c786a876ac81d36b2dc236
harrisonvijay <vijay.harrison@gmail.com>
2021-06-25 19:40:04 +0530

    Restricted smaller devices

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <PathfindingVisualizer />
-    </div>
+    <>
+      <div className="App">
+        <PathfindingVisualizer />
+      </div>
+      <div className="overlay">
+        <span>This app is only supported in displays with width greater than 1200px</span>
+      </div>
+    </>
   );
 }
 
